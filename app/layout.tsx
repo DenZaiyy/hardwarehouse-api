232b49bb70fcedcdabe8d5308958f1168cf5e86e
DenZaiyy <denzaiyy@outlook.fr>
2025-09-18 14:16:37 +0200

    fix: main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,20 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "HardWareHouse - API",
-  description: "API to manage products and categories for a hardware store",
+    title: "HardWareHouse - API",
+    description: "API to manage products and categories for a hardware store",
+    authors: {name: 'Your Name', url: 'https://yourwebsite.com'},
+    keywords: ['Hardware', 'Store', 'Products', 'Categories', 'API', 'Next.js', 'TypeScript', 'Prisma', 'RESTful'],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="fr">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+export default function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="fr">
+            <body className={`antialiased`}>
+                {children}
+            </body>
+        </html>
+    );
 }
