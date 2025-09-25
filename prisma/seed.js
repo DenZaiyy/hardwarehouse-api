ad1e37033b62d987727ef2cd37c10f3c12aab664
DenZaiyy <denzaiyy@outlook.fr>
2025-09-25 08:38:10 +0200

    fix: ignore eslint errors

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const { PrismaClient } = require('../app/generated/prisma');
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const slugify = require('slugify');
 
 const prisma = new PrismaClient();
