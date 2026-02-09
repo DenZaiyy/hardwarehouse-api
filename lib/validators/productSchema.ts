d49e5584d59ac81bb99f271a142c79ef43705eaa
DenZaiyy <denzaiyy@outlook.fr>
2026-02-09 13:54:03 +0100

    fix: zod schemas

--- lib/validators/productSchema.ts
+++ lib/validators/productSchema.ts
@@ -5,20 +5,14 @@ export const productSchema = z.object({
     price: z.coerce.number<number>().min(0, "Le prix doit être un nombre positif"),
     description: z.string().optional(),
     shortDescription: z.string().max(255, "La description courte ne doit pas dépasser 255 caractères").optional(),
-    thumbnail: z.union([
-        z.instanceof(File, {message: "Le fichier de la miniature doit être un fichier valide"})
-            .refine(file => !file || file.size !== 0 || file.size <= 5000000, {message: 'La taille maximum a été dépassé'} ),
-        z.string().optional(),
-    ])
-        .refine(value => value instanceof File || typeof value === 'string', {
-        message: "La miniature doit être un fichier ou une URL valide"
-        }).optional(),
-    images: z.union([
-        z.array(z.instanceof(File)),
-        z.array(z.string().url("L'URL de l'image doit être valide"))
-    ]).optional(),
+    thumbnail: z.instanceof(File, {message: "Le fichier de la miniature doit être un fichier valide"})
+        .refine(file => file.size <= 5000000, {message: 'La taille maximum a été dépassé'})
+        .optional(),
+    images: z.array(z.instanceof(File, {message: "Les fichiers doivent être valides"}))
+        .refine(files => files.every(file => file.size <= 5000000), {message: 'La taille maximum a été dépassée pour une ou plusieurs images'})
+        .optional(),
     attributes: z.record(z.string(), z.string()).optional(),
     active: z.boolean(),
     brandId: z.string().min(1, "La marque est requise"),
-    categoryId: z.string().min(1, "La catégorie est requise"),
+    category: z.string().min(1, "La catégorie est requise"),
 })
